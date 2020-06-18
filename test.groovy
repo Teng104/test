82c90e72310e13c9b25e38bf12c92af7c7f23c76
colin_teng <colin_teng@edge-core.com>
2020-06-18 10:41:54 +0800

    Branch variable test - add script

--- test.groovy
+++ test.groovy
@@ -3,10 +3,12 @@ pipeline {
     stages{
         stage("test") {
             steps {
-                echo "Hi, dev"
-                echo env.GIT_BRANCH
-		git_branch = env.GIT_BRANCH.split("/")[env.GIT_BRANCH.split("/").length - 1]
-		echo git_branch
+	        script {
+                    echo "Hi, dev"
+                    echo env.GIT_BRANCH
+		    git_branch = env.GIT_BRANCH.split("/")[env.GIT_BRANCH.split("/").length - 1]
+		    echo git_branch
+		}
             }
         }
     }
