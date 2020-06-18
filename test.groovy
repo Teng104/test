64e6d0d13806ceca735932ae5182410c2ec50661
colin_teng <colin_teng@edge-core.com>
2020-06-18 13:32:53 +0800

    Merge branch variable

--- test.groovy
+++ test.groovy
@@ -3,9 +3,18 @@ pipeline {
     stages{
         stage("test") {
             steps {
+<<<<<<< HEAD
                 echo "Hi"
                 echo "GIT_BRANCH: ${env.GIT_BRANCH}"
                 echo env.GIT_LOCAL_BRANCH 
+=======
+	        script {
+                    echo "Hi, dev"
+                    echo env.GIT_BRANCH
+		    git_branch = env.GIT_BRANCH.split("/")[env.GIT_BRANCH.split("/").length - 1]
+		    echo git_branch
+		}
+>>>>>>> 82c90e7... Branch variable test - add script
             }
         }
     }
