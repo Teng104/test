fb6e90e6fd2a5f47652e35562a9b8b0f97e81a39
colin_teng <colin_teng@edge-core.com>
2020-06-19 09:59:05 +0800

    Modify is_master type

--- test.groovy
+++ test.groovy
@@ -1,13 +1,13 @@
 pipeline {
     environment {
-        is_master = (env.GIT_BRANCH != "origin/dev")
+        is_master = (env.GIT_BRANCH != "origin/dev") ? "true" : "false"
     }
     agent { label "master" }
     stages{
         stage("test") {
             steps {
 	        script {
-		    str = (is_master) ? "master" : "dev"
+		    str = (is_master.toBoolean()) ? "master" : "dev"
                     echo "Hi, ${str}"
                     echo env.GIT_BRANCH
 		    git_branch = env.GIT_BRANCH.split("/")[env.GIT_BRANCH.split("/").length - 1]
