72d7ca7ccde37eb0527364a9fa6c151a1efc1513
colin_teng <colin_teng@edge-core.com>
2020-06-19 10:03:15 +0800

    Remove environment variable and add isMaster function

--- test.groovy
+++ test.groovy
@@ -1,13 +1,13 @@
+def isMaster() {
+    return (env.GIT_BRANCH != "origin/dev")
+}
 pipeline {
-    environment {
-        is_master = (env.GIT_BRANCH != "origin/dev") ? "true" : "false"
-    }
     agent { label "master" }
     stages{
         stage("test") {
             steps {
 	        script {
-		    str = (is_master.toBoolean()) ? "master" : "dev"
+		    str = isMaster() ? "master" : "dev"
                     echo "Hi, ${str}"
                     echo env.GIT_BRANCH
 		    git_branch = env.GIT_BRANCH.split("/")[env.GIT_BRANCH.split("/").length - 1]
