4c0f4d577ba7eda213fa4f88af313bb7db1dc565
colin_teng <colin_teng@edge-core.com>
2020-06-19 13:45:50 +0800

    Replace tab with space

--- test.groovy
+++ test.groovy
@@ -1,22 +1,20 @@
 def isMaster() {
     return (env.GIT_BRANCH != "origin/dev")
 }
-jubSuffix = isMaster() ? "" : "-Dev"
 pipeline {
     agent { label "master" }
     stages{
         stage("test") {
             steps {
-	        script {
-		    job_suffix = isMaster() ? "" : "-Dev"
-		    echo jubSuffix
-		    //build job: "OpenBMC${jubSuffix}/Colin-Branch-Sub-Test${jubSuffix}"
-		    str = isMaster() ? "master" : "dev"
+                script {
+                    job_suffix = isMaster() ? "" : "-Dev"
+                    echo "OpenBMC${job_suffix}/Colin-Branch-Sub-Test${job_suffix}"
+                    str = isMaster() ? "master" : "dev"
                     echo "Hi, ${str}"
                     echo env.GIT_BRANCH
-		    git_branch = env.GIT_BRANCH.split("/")[env.GIT_BRANCH.split("/").length - 1]
-		    echo git_branch
-		}
+                    git_branch = env.GIT_BRANCH.split("/")[env.GIT_BRANCH.split("/").length - 1]
+                    echo git_branch
+                }
             }
         }
     }
