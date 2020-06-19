232fe466349b37b977d91852b0f51227f00b26d6
colin_teng <colin_teng@edge-core.com>
2020-06-19 09:55:02 +0800

    Add is_master

--- test.groovy
+++ test.groovy
@@ -1,10 +1,14 @@
 pipeline {
+    environment {
+        is_master = (env.GIT_BRANCH != "origin/dev")
+    }
     agent { label "master" }
     stages{
         stage("test") {
             steps {
 	        script {
-                    echo "Hi, dev"
+		    str = (is_master) ? "master" : "dev"
+                    echo "Hi, ${str}"
                     echo env.GIT_BRANCH
 		    git_branch = env.GIT_BRANCH.split("/")[env.GIT_BRANCH.split("/").length - 1]
 		    echo git_branch
