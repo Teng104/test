b5c9d919e2eaea3fa6d6cbf8834abf7701ca3760
colin_teng <colin_teng@edge-core.com>
2020-06-19 10:37:42 +0800

    Change job_suffix to local variable

--- test.groovy
+++ test.groovy
@@ -1,16 +1,14 @@
 def isMaster() {
     return (env.GIT_BRANCH != "origin/dev")
 }
-def jobSuffix = ""
-jobsuffix = isMaster() ? "" : "-Dev"
 pipeline {
     agent { label "master" }
     stages{
         stage("test") {
             steps {
 	        script {
-		    echo "${jobSuffix}"
-		    build job: "OpenBMC${jobSuffix}/Colin-Branch-Sub-Test${jobSuffix}"
+		    job_suffix = isMaster() ? "" : "-Dev"
+		    build job: "OpenBMC${job_suffix}/Colin-Branch-Sub-Test${job_suffix}"
 		    str = isMaster() ? "master" : "dev"
                     echo "Hi, ${str}"
                     echo env.GIT_BRANCH
